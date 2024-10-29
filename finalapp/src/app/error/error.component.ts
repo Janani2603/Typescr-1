d58d8b7406db63c04deb9bb50e21d192f36f9185
janani <gjanani2003@gmail.com>
2024-10-29 16:05:26 +0530

    new

--- finalapp/src/app/error/error.component.ts
+++ finalapp/src/app/error/error.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
 export class ErrorComponent {
   message:string='something went wrong,pl try after some time';
   code:number=500;
+  currentDate: Date = new Date(); 
+  normaltext: string | number = 'hey alll!!!!'; 
+  gst: number = 12.87;
+  actualprice: number = 1994.5678;
 
 }
